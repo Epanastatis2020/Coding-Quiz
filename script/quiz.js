9163917fe13bbef88bb3ec7e285f2a7ae719ade8
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-09-09 17:31:13 +1000

    consolidated html to one page instead of multiple pages. Added placeholder array for quiz questions

---
+++ script/quiz.js
@@ -0,0 +1,8 @@
+
+//array with questions, answers, correct answers
+var questions = [
+    {
+        questionTitle: "What does HTML stand for?",
+        answerChoices:["Home Tool Marking Language" , "Hypertext Markup Language", "Hyperlinks and Text Markup Language", "Hypertext Machine Language"],
+        correctAnswer: 1
+    };
